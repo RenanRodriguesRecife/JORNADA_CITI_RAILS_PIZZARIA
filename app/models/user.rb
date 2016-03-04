be5d219b599fde8083f8388bd5308462b4fe4ab6
Renan Rodrigues <renan_ime_2012@hotmail.com>
2016-03-04 17:19:14 -0300

    validação tel

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
 	has_many :offers_users
 	has_many :offers, :through => :offers_users
 	validates :name, presence: true, length: {maximum: 50}
-	validates :tel, presence: true, length: {maximum: 50}
+	validates :tel, presence: true, length: {in:8..10}
 	validates :address, presence: true, length: {maximum: 50}
 end
